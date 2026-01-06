a51b3dcf3147c16f453a5ec095f18f1f43aa7593
97solomio <krishna.pahan00@gmail.com>
2026-01-06 20:51:01 +0600

    initial commit

---
+++ lms/lmsapp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Course)
+admin.site.register(Enrollment)
+admin.site.register(Lesson)
+admin.site.register(Assignment)
+admin.site.register(Submission)
+admin.site.register(Results)
